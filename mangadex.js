bc6f3f449b678ed4d827e0006a2b16d81b09757a
Vincent Wong <vincentwong5609@gmail.com>
2024-10-23 22:35:37 -0700

    finished get manga list function for mangadex extension

--- mangadex.js
+++ mangadex.js
@@ -1,54 +1,46 @@
 const axios = require('axios');
 
-const baseURL = 'https://api.mangadex.org'
+const baseURL = 'https://api.mangadex.org';
+const baseCoverURL = 'https://uploads.mangadex.org/covers'
 
 const getMangaList = async (title) => {
     try {
         // Gets a list of related manga based on the title
-        const res = await axios.get(`${baseURL}/manga`, {
+        const res = await axios.get(`${baseURL}/manga?includes[]=cover_art`, {
             params: {
                 title: title
             }
-        })
-        // Maps out all the ids into a list
-        const mangaIds = res.data.data.map(manga => manga.id)
+        });
 
-        // Calls getMangaDetails function which is asynchronous for each mangaIds' list item
-        const mangaDetailsPromises = mangaIds.map(async (id) => {
-            const details = await getMangaDetails(id)
-            return details.data
-        })
-        // Tells code to wait for all promise to finish before continuing
-        const mangaDetails = await Promise.all(mangaDetailsPromises)
-        
+        // Create an array of manga objects containing id, title, and cover_art filename
+        const mangaList = res.data.data.map(manga => {
+            const relationships = manga.relationships;
 
-    } catch(error) {
-        console.error(error)
+            // Find cover_art attributes
+            const coverArt = relationships
+                .filter(relationship => relationship.type === 'cover_art')
+                .map(cover => cover.attributes);
+
+            // Define variables outside the return statement
+            const id = manga.id;
+            const title = manga.attributes.title.en; // Ensure to extract the title correctly
+            const cover_art_filename = coverArt.length > 0 ? coverArt[0].fileName : null; // Return the filename or null if none exists
+            const coverURL = `${baseCoverURL}/${id}/${cover_art_filename}`
+
+            return {
+                id,
+                title,
+                cover_art_filename,
+                coverURL
+            };
+        });
+
+        return mangaList; // Return the array of manga objects
+
+    } catch (error) {
+        console.error(error);
+        return []; // Return an empty array in case of error
     }
+};
 
-}
-
-// Function to get manga details from manga id  
-const getMangaDetails = async (id) => {
-    try {
-        const res = await axios.get(`${baseURL}/manga/${id}`)
-        return res
-    } catch(error) {
-        console.error(error)
-    }
-    
-
-}
-
-// Function to get author from author id
-const getAuthor = async (id) => {
-    try{
-        const res = await axios.get(`${baseURL}/author/${id}`)
-        return res
-    } catch(error) {
-        console.error(error)
-    }
-}
-
-// Test function
-getMangaList("Dragon Ball")
+module.exports = getMangaList;
