1a4bb9720e83ba8520f4cb11f1ccdbee11231d55
Vincent Wong <vincentwong5609@gmail.com>
2024-10-27 17:59:03 -0700

    started extensions

--- frontend/src/pages/Extensions/Extensions.jsx
+++ frontend/src/pages/Extensions/Extensions.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import "../../Normalize.css";
+import "./Extensions.css";
+import { Nav } from "../../components/Nav/Nav";
+import { Library } from "../../components/Library/Library";
+import { ExtensionsList } from "../../components/ExtensionList/ExtensionsList";
 
 export const Extensions = () => {
-    return;
+    return (
+        <div id="app">
+            <Nav />
+            <ExtensionsList />
+            <section id="details"></section>
+        </div>
+    );
 };
