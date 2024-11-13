7cfc8697cf918137e5d15c652e4fc889092c63b1
Vincent Wong <vincentwong5609@gmail.com>
2024-11-13 13:15:51 -0800

    started extensions component

--- frontend/src/components/ExtensionList/ExtensionsList.jsx
+++ frontend/src/components/ExtensionList/ExtensionsList.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import '../../Normalize.css';
 import './ExtensionsList.css';
+import { InstalledExtensions } from '../InstalledExtensions/InstalledExtensions';
 
 export const ExtensionsList = () => {
-	return <div id='extensions-component'></div>;
+	return (
+		<div id='extensions-component'>
+			<InstalledExtensions />
+		</div>
+	);
 };
---
+++ frontend/src/components/InstalledExtensions/InstalledExtensions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../Normalize.css';
+import './InstalledExtensions.css';
+
+export const InstalledExtensions = () => {
+	return (
+		<div id='installed-extensions'>
+			<h2>Manage</h2>
+		</div>
+	);
+};
