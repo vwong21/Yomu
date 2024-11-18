586a4653ed7dc5d7fff8d392dd532150dee56077
Vincent Wong <vincentwong5609@gmail.com>
2024-11-17 21:15:01 -0800

    added comments for extensionCard

--- frontend/src/components/common/ExtensionCard/ExtensionCard.jsx
+++ frontend/src/components/common/ExtensionCard/ExtensionCard.jsx
@@ -2,15 +2,19 @@ import React, { useEffect, useState } from 'react';
 import '../../../Normalize.css';
 import './ExtensionCard.css';
 
+// Component that returns a card for each extension
 export const ExtensionCard = ({ name, description, image, installed }) => {
+	// State to see if the extension is installed. If it is, it will display a manage button instead of an install button and vice versa
 	const [isInstalled, setIsInstalled] = useState(installed);
 
+	// Refresh the page whenever the state is changed so that if the user installs or deletes an extension, the button that is rendered changes right away
 	useEffect(() => console.log(isInstalled), [isInstalled]);
 
+	// Function that calls the downloadExtension function from the IPC which downloads the selected extension
 	const installExtension = async (extensionName) => {
 		try {
+			// Call to IPC
 			const res = await window.api.downloadExtension(extensionName);
-			console.log(res);
 			if (res.status == 'success') {
 				setIsInstalled(true);
 			}
@@ -28,6 +32,7 @@ export const ExtensionCard = ({ name, description, image, installed }) => {
 				<p className='extension-title'>{name}</p>
 				<p className='extension-details'>{description}</p>
 				<div className='extension-action-container'>
+					{/* If the extension is installed, display a manage button, else display an install button */}
 					{isInstalled && <p className='extension-action'>Manage</p>}
 					{!isInstalled && (
 						<p
