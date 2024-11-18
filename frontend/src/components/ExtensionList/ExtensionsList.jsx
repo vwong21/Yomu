42a0ce7ee50cff412b53942a4ac3e21793afdf45
Vincent Wong <vincentwong5609@gmail.com>
2024-11-18 13:34:58 -0800

    consolidated installed extensions component to extensions list

--- frontend/src/components/ExtensionList/ExtensionsList.jsx
+++ frontend/src/components/ExtensionList/ExtensionsList.jsx
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../Normalize.css';
 import './ExtensionsList.css';
-import { InstalledExtensions } from '../InstalledExtensions/InstalledExtensions';
+import { ExtensionCard } from '../common/ExtensionCard/ExtensionCard';
 
 export const ExtensionsList = () => {
+	// State to either show all the extensions or only the installed ones. False will show all extensions
+	const [filterExtensions, setFilterExtensions] = useState(false);
+	// State that will be an array of all extensions. This array may be filtered depending on the filterExtensions state
+	const [installedExtensions, setInstalledExtensions] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	// Function to make call to retrieveExtensions function through IPC
+	const fetchExtensions = async () => {
+		try {
+			const res = await window.api.retrieveExtensions();
+			setInstalledExtensions(res);
+			setLoading(false);
+			console.log(res);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	// UseEffect to refresh the renderer at the beginning and then fetch all the extensions
+	useEffect(() => {
+		fetchExtensions();
+	}, []);
 	return (
 		// Sets background for the extensionList. This includes color and sizing
 		<div id='extensions-component'>
-			<InstalledExtensions />
+			<div id='installed-extensions'>
+				<h2>Manage</h2>
+				{loading && <p>Loading</p>}
+				{!loading && (
+					<div id='extensions-list-container'>
+						{/* Calls the ExtensionCard component for each of the extensions in installedExtensions */}
+						{installedExtensions.map((extension, index) => (
+							<ExtensionCard
+								key={index}
+								name={extension.name}
+								description={extension.description}
+								image={extension.image}
+								installed={extension.installed}
+							/>
+						))}
+					</div>
+				)}
+			</div>
 		</div>
 	);
 };
