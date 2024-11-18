0dc25070161033399e7dddce4171eb9e6a804b3b
Vincent Wong <vincentwong5609@gmail.com>
2024-11-17 21:25:33 -0800

    added comments for extensionList and its css file

--- frontend/src/components/ExtensionList/ExtensionsList.jsx
+++ frontend/src/components/ExtensionList/ExtensionsList.jsx
@@ -5,6 +5,7 @@ import { InstalledExtensions } from '../InstalledExtensions/InstalledExtensions'
 
 export const ExtensionsList = () => {
 	return (
+		// Sets background for the extensionList. This includes color and sizing
 		<div id='extensions-component'>
 			<InstalledExtensions />
 		</div>
