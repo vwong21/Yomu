f67a8c1c4360161e492b20f925557a3323bc5912
Vincent Wong <vincentwong5609@gmail.com>
2024-11-28 12:11:29 -0800

    changed css to modules for Home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import '../../Normalize.css';
-import './Home.css';
+import styles from './Home.module.css';
 import { Nav } from '../../components/Nav/Nav';
 import { Library } from '../../components/Library/Library';
 import { Frame } from '../../components/Frame/Frame';
 
 export const Home = () => {
 	return (
-		<div id='app'>
+		<div id={styles.app}>
 			<Frame />
 			{/* 3 sections, navbar, library, and details */}
 			<Nav />
 			<Library />
-			<section id='details'></section>
+			<section id={styles.details}></section>
 		</div>
 	);
 };
