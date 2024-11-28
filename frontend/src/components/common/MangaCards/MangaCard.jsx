b5fb3d97a4f4e728eb8b1e836b191a107e51b608
Vincent Wong <vincentwong5609@gmail.com>
2024-11-28 11:57:26 -0800

    changed css to modules for mangacards

--- frontend/src/components/common/MangaCards/MangaCard.jsx
+++ frontend/src/components/common/MangaCards/MangaCard.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import '../../../Normalize.css';
-import './MangaCard.css';
+import styles from './MangaCard.module.css';
 
 export const MangaCard = ({ name, author, image }) => {
 	// Takes name, author, image passed from Library.jsx for the individual manga cards.
 	return (
-		<div id='manga-card'>
-			<div id='card-container'>
-				<div id='image-container' className='card-container-children'>
-					<img className='manga-cover' src={image} alt='' />
+		<div id={styles.mangaCard}>
+			<div id={styles.cardContainer}>
+				<div
+					id={styles.imageContainer}
+					className={styles.cardContainerChildren}>
+					<img className={styles.mangaCover} src={image} alt='' />
 				</div>
 
-				<div id='text-container' className='card-container-children'>
-					<p id='manga-title'>{name}</p>
-					<p id='manga-author'>{author}</p>
+				<div
+					id={styles.textContainer}
+					className={styles.cardContainerChildren}>
+					<p id={styles.mangaTitle}>{name}</p>
+					<p id={styles.mangaAuthor}>{author}</p>
 				</div>
 			</div>
 		</div>
