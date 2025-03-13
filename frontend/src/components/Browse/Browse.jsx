df7117bf8224688a1f85841749c28d38d1849ecd
Vincent Wong <vincentwong5609@gmail.com>
2025-03-13 14:23:26 -0700

    added load more button in frontend. Need to load them in order now

--- frontend/src/components/Browse/Browse.jsx
+++ frontend/src/components/Browse/Browse.jsx
@@ -4,26 +4,30 @@ import styles from './Browse.module.css';
 import { MangaCardBrowse } from '../common/MangaCardBrowse/MangaCardBrowse';
 
 export const Browse = ({ selectedExtension }) => {
-	console.log(`from Browse: ${selectedExtension}`);
+	const [loading, setLoading] = useState(false);
 	const [mangaObj, setMangaObj] = useState([]);
 
-	useEffect(() => {
-		const browseManga = async () => {
-			try {
-				const res = await window.api.browseManga(selectedExtension);
-				console.log(res);
-				if (Array.isArray(res)) {
-					setMangaObj(res);
-				}
-			} catch (error) {
-				console.error(error);
+	const browseManga = async () => {
+		try {
+			setLoading(true);
+			const res = await window.api.browseManga(selectedExtension);
+			console.log(res);
+			if (Array.isArray(res)) {
+				setMangaObj((prev) => [...prev, ...res]);
 			}
-		};
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+	useEffect(() => {
 		browseManga();
 	}, [selectedExtension]);
 
 	return (
 		<div id={styles.browseContainer}>
+			{loading && <p id={styles.loading}>Loading...</p>}
 			<div id={styles.gridContainer}>
 				{mangaObj.length > 0 &&
 					mangaObj.map((manga, index) => {
@@ -37,6 +41,15 @@ export const Browse = ({ selectedExtension }) => {
 							/>
 						);
 					})}
+				{mangaObj.length > 0 && (
+					<p
+						id={styles.loadMore}
+						onClick={() => {
+							browseManga();
+						}}>
+						Load More...
+					</p>
+				)}
 			</div>
 		</div>
 	);
--- frontend/src/components/common/MangaCardBrowse/MangaCardBrowse.jsx
+++ frontend/src/components/common/MangaCardBrowse/MangaCardBrowse.jsx
@@ -3,7 +3,6 @@ import '../../../Normalize.css';
 import styles from './MangaCardBrowse.module.css';
 
 export const MangaCardBrowse = ({ id, title, coverArt }) => {
-	console.log(id, title, coverArt);
 	return (
 		<div className={styles.mangaCardBrowseContainer}>
 			<img className={styles.coverArt} src={coverArt} alt='' />
